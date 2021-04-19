07f1b59319052c29c75526a848933c1d7dab96ff
william <william810212@gmail.com>
2021-04-19 23:27:45 +0800

    install headlessui/vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="py-16">
+    <Switch
+      v-model="enabled"
+      :class="enabled ? 'bg-blue-500' : 'bg-blue-300'"
+      class="relative inline-flex flex-shrink-0 h-[38px] w-[74px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+      <span class="sr-only">Use setting</span>
+      <span
+        aria-hidden="true"
+        :class="enabled ? 'translate-x-9' : 'translate-x-0'"
+        class="pointer-events-none inline-block h-[34px] w-[34px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200" />
+    </Switch>
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { ref } from "vue";
+import { Switch } from "@headlessui/vue";
+
+export default {
+  components: { Switch },
+
+  setup() {
+    const enabled = ref(false);
+
+    return { enabled };
+  },
+};
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
