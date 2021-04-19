a8499e043b47e5ee1d7e29d335195caabf830b10
william <william810212@gmail.com>
2021-04-19 23:01:13 +0800

    install tailwindcss, autoprefixer, postcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',  
+  purge: [
+    './index.html',
+    './src/**/*.{vue,js}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
